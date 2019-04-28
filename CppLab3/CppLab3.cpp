611a3bd3142ca9306928abe91d407966f15a6034
TheOrangeInd <TheOrangeInd@DESKTOP-AAO5NUL>
2019-04-28 19:50:01 +0300

    ok

--- CppLab3/CppLab3.cpp
+++ CppLab3/CppLab3.cpp
@@ -1,21 +1,76 @@
-﻿// CppLab3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include "pch.h"
+﻿#include "pch.h"
 #include <iostream>
 
+using namespace std;
+
+//variant 8
+
+//mid difficulty level
+/*
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	const int length = 30;
+	char c[length] = "*He***l*l***o wor**l*d**!";
+	char s[2*length];
+	int count = 0;
+
+	cout << c << "\n";
+
+	for (int i = 0; i < sizeof(c) / sizeof(char); i++)
+	{
+		if (c[i] != '*')
+		{
+			s[i + count] = c[i];
+			s[i + count + 1] = c[i];
+			count++;
+		}
+		else
+		{
+			count--;
+		}
+	}
+
+	cout << count << "\n";
+	cout << s;
+
+	return 0;
 }
+*/
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+//high difficulty level
+int main()
+{
+	char c[] = "QWERTYUIOP";
+	char s[] = "PIQEOUWRYT";
+
+	if (sizeof(c) != sizeof(s))
+	{
+		cout << "NOT ANAGRAMS";
+		return 0;
+	}
+	else
+	{
+		char t;
+
+		for (int i = 0; i < sizeof(c) / sizeof(char); i++)
+		{
+			for (int j = i + 1; j < sizeof(c) / sizeof(char); j++)
+			{
+				if (c[i] != s[i])
+				{
+					t = s[i];
+					s[i] = s[j];
+					s[j] = t;
+				}
+			}
+		}
+	}
+
+	cout << c << "\n" << s << "\n";
+
+	if (strcmp(c, s) == 0) cout << "ANAGRAMS";
+	else cout << "NOT ANAGRAMS";
+
+	return 0;
+}
